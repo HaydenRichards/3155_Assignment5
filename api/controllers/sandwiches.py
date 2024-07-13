900150a93bed9e121967ceb28fb46129a8db8a16
Hayden Richards <richardshayden12@gmail.com>
2024-07-13 15:55:06 -0400

    feat: update to sandwiches.py based on schemas

--- api/controllers/sandwiches.py
+++ api/controllers/sandwiches.py
@@ -6,7 +6,7 @@ from ..models import models, schemas
 def create(db: Session, sandwich):
     # Create a new instance of the Sandwich model with the provided data
     db_sandwich = models.Sandwich(
-        size=sandwich.size,
+        sandwich_name=sandwich.sandwich_name,
         price=sandwich.price
     )
     # Add the newly created Sandwich object to the database session
